917d9e64d3664f9396c36a9bda8ac6d04d18f1a2
DESKTOP-5OJB6B7\baba <jalarez2000@gmail.com>
2020-11-05 12:25:22 +0100

    Dit is de Autogame/Challenge1game Unit1

---
+++ Unit 1/Unit 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 10f;
+    private float rotationSpeed = 50f;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        // get the user's input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+    }
+}
