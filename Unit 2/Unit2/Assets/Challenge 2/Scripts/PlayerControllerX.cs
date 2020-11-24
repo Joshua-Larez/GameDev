95777f46584d4c73d83b0bd81f7f6353b9a842b6
DESKTOP-5OJB6B7\baba <jalarez2000@gmail.com>
2020-11-24 09:32:33 +0100

    Dit is de Shooting-foodGame/Challenge2Game Unit2

---
+++ Unit 2/Unit2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    //update is called once per frame
+    void Update()
+    {
+        //shoot dog everytime space is clicked
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
+
+
+
+
+    // I tried to make delay but dont know how this "onder" i have, but i dont know so i just left it for someone curios
+
+// Update is called once per frame
+//void Update()
+//{
+// On spacebar press, send dog
+//if (Input.GetKeyDown(KeyCode.Space))
+// {
+// StartCoroutine(ShootDogDelay());
+// }
+//}
+
+//IEnumerator ShootDogDelay()
+//{
+//yield return new WaitForSeconds(2);
+//Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+//}
+//}
---
+++ Unit 2/Unit2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    private float speed = 40.0f;//how fast the player can move
+    private float xRange = 30;//the range the player can move 
+
+    //cookie 
+    public GameObject projectilePrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player moving left
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        //player moving rigth
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        //the axis that the player moves
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //shoot cookies from player position
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
+    }
+}
